31582a97d641dc449ac477f56a9c0ffcd8218371
WangDa <wang.d@scopicsoftware.com>
2019-11-20 04:16:34 +1000

    Set background color if node is matched to search text

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -11,6 +11,7 @@ export class TreeComponent implements OnInit {
 
   @Input() public height: number;
   public searchtext: string = '';
+  public result: string = '';
 
   treeControl = new NestedTreeControl<any>(node => node.children);
   fromDataSource = new MatTreeNestedDataSource<any>();
@@ -134,48 +135,77 @@ export class TreeComponent implements OnInit {
   }
 
   searchTree(event: KeyboardEvent) {
-    let temp = JSON.parse(JSON.stringify(this.FROM_DATA)), filtered = [];
+    let temp = JSON.parse(JSON.stringify(this.fromDataSource.data));
 
     for (let i = 0; i < temp.length; i++) {
-      this.searchNode(temp[i]);
-      if (temp[i].children.length !== 0) {
-        filtered.push(temp[i]);
+      let node = temp[i];
+      if (node.children.length !== 0) {
+        this.searchNode(node);
+      }
+      if (node.name.toLowerCase().match(this.searchtext.toLowerCase())) {
+        node.searched = true;
+      } else {
+        node.searched = false;
+      }
+      if (this.searchtext === '' || this.searchtext === null) {
+        node.searched = false;
       }
     }
-    this.fromDataSource.data = JSON.parse(JSON.stringify(filtered));
+    this.fromDataSource.data = JSON.parse(JSON.stringify(temp));
+    console.log(this.fromDataSource.data)
   }
 
   searchNode(root) {
     if ("children" in root) {
       let children = root.children;
       let len = children.length;
-      var newChildren = [];
       for (let i = 0; i < len; i++) {
         let child = children[i];
         if ("children" in child) {
           // this is not a leaf
-          if (child.name.match(this.searchtext)) {
-            newChildren.push(child);
-          }
-          else {
-            this.searchNode(child);
-            if (child.children.length !== 0) {
-              newChildren.push(child);
-            }
-          }
+          this.searchNode(child);
         }
-        else {
-          //this is a leaf
-          if (child.name.match(this.searchtext)) {
-            newChildren.push(child);
-          }
+        if (child.name.toLowerCase().match(this.searchtext.toLowerCase())) {
+          child.searched = true;
+        } else {
+          child.searched = false;
+        }
+        if (this.searchtext === '' || this.searchtext === null) {
+          child.searched = false;
         }
       }
-      root.children = newChildren;
     }
   }
 
-  showToList() {}
+  showToList() {
+    let temp = JSON.parse(JSON.stringify(this.toDataSource.data));
+    this.result = 'Selected Items: ';
+
+    for (let i = 0; i < temp.length; i++) {
+      if (temp[i].children.length !== 0) {
+        this.showNodeName(temp[i]);
+      } else {
+        this.result += temp[i].name + ', ';
+      }
+    }
+    this.result = this.result.substring(0, this.result.length-2);
+  }
+
+  showNodeName(root) {
+    if ('children' in root) {
+      let children = root.children;
+      let len = children.length;
+      for (let i = 0; i < len; i++) {
+        if ('children' in children[i]) {
+          this.showNodeName(children[i]);
+        } else {
+          this.result += children[i].name + ', ';
+        }
+      }
+    } else {
+      this.result += root.name + ', ';
+    }
+  }
 
   hasChild = (_: number, node: any) => !!node.children && node.children.length > 0;
 
